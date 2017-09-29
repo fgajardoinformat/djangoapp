d0401e085bcc8ea51f95358f2855fff2ff02ff86
Flavio Gajardo <fgajardo@informat.cl>
2017-09-29 21:58:15 +0000

    proyecto inicial

--- myapp/polls/admin.py
+++ myapp/polls/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import Preguntas
+from .models import Opciones
+
+admin.site.register(Preguntas)
+admin.site.register(Opciones)
+
 # Register your models here.
